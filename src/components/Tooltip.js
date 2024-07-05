6bc94081876e43ce1331a6fb0c42772cd143e152
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-05 11:56:31 +0530

    done

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../styles/App.css";
-
 const Tooltip = ({ children, text }) => {
+  const [visible, setVisible] = useState(false);
+
+  const showTooltip = () => setVisible(true);
+  const hideTooltip = () => setVisible(false);
+
   return (
-    <div className="tooltip">
+    <div
+      className="tooltip-container"
+      onMouseEnter={showTooltip}
+      onMouseLeave={hideTooltip}
+    >
       {children}
-      <span className="tooltiptext">{text}</span>
+      {visible && <div className="tooltip-text">{text}</div>}
     </div>
   );
 };
